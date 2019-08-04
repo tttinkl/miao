a1b1976f53216aba847fc1fba098ac4be932f206
tttinkl <tttinkl@163.com>
2019-08-04 21:59:14 +0800

    lodash

--- lodash/tttinkl-lodash.js
+++ lodash/tttinkl-lodash.js
@@ -1,5 +1,49 @@
-var tttinkl = {
-  compact: function(ary) {
-    return ary.filter(it => it)
-  },
-}
+var tttinkl = function() {
+    function chunk(ary,n) {
+      var ret = [];
+      while(ary.length != 0) {
+        ret.push(ary.splice(0,n));
+      }
+      return ret;
+    }
+
+    function compact(ary) {
+      return ary.filter(it => it)
+    }
+
+    function concat(array,values) {
+      return ary.concat(...values)
+    }
+
+    function diffrence(ary,values) {
+
+    }
+
+    function diffrenceBy(ary,values) {
+
+    }
+
+    function drop(ary,n = 1) {
+      ary.splice(0,n);
+      return ary;
+    }
+
+    function dropRight(array,n = 1) {
+      ary.splice(ary.length - n);
+      return ary;
+    }
+
+    dropRightWhile(array,)
+
+
+
+    return {
+      chunk,
+      compact,
+      concat,
+      diffrence,
+      diffrenceBy,
+      drop,
+      dropRight,
+    }
+}();
